3c69f7de137d975a6b54625ac0ae2c3f7d21e4ba
podonoghue <podonoghue@swin.edu.au>
2016-09-29 08:08:58 +1000

    V1.0.0 Changed to multi-key method

--- Firmware/SMT_Oven/Sources/switch_debouncer.h
+++ Firmware/SMT_Oven/Sources/switch_debouncer.h
@@ -9,14 +9,7 @@
 #define SOURCES_SWITCH_DEBOUNCER_H_
 
 /**
- * @tparam timerChannel The PIT channel number to use for callback
- * @tparam f1           F1 switch
- * @tparam f2           F2 switch
- * @tparam f3           F3 switch
- * @tparam f4           F4 switch
- * @tparam sel          S  switch
- *
- * F1..F4 have auto-repeat function
+ * Return values from switch debouncer
  */
 enum SwitchValue {
    SW_NONE = 0,
@@ -25,83 +18,76 @@ enum SwitchValue {
    SW_F3   = 1<<2,
    SW_F4   = 1<<3,
    SW_S    = 1<<4,
+   SW_F3F4 = SW_F3|SW_F4, // used for +/- keys together
 };
 
+/**
+ * @tparam timerChannel The PIT channel number to use for callback
+ * @tparam f1           F1 switch GPIO
+ * @tparam f2           F2 switch GPIO
+ * @tparam f3           F3 switch GPIO
+ * @tparam f4           F4 switch GPIO
+ * @tparam sel          S  switch GPIO
+ *
+ * F1..F4 have auto-repeat function
+ */
 template<unsigned timerChannel, typename f1, typename f2, typename f3, typename f4, typename sel>
 class SwitchDebouncer {
 private:
    // Time to debounce the switch (ms)
-   static constexpr int DEBOUNCE_THRESHOLD = 20;
+   // This also affects how easy it is to do intentional multi-presses
+   static constexpr int DEBOUNCE_THRESHOLD = 50;
+
    // Auto-repeat delay (ms)
    static constexpr int REPEAT_THRESHOLD   = 1000;
+
    // Auto-repeat period (ms)
    static constexpr int REPEAT_PERIOD      = 200;
 
    // Last pressed switch
    static volatile SwitchValue switchNum;
 
+   // Indicates that the key is repeating
+   static volatile bool repeating;
+
    /**
     * Called at a regular rate to poll the switches
     */
    static void switchHandler(void) {
-      static uint sw1Count = 0;
-      static uint sw2Count = 0;
-      static uint sw3Count = 0;
-      static uint sw4Count = 0;
-      static uint sw5Count = 0;
+      static uint debounceCount = 0;
+      static uint lastSnapshot  = 0;
 
-      if (!f1::read()) {
-         sw1Count++;
-         if ((sw1Count == DEBOUNCE_THRESHOLD) || ((sw1Count >= REPEAT_THRESHOLD) && ((sw1Count % REPEAT_PERIOD) == 0))) {
-            switchNum = SW_F1;
+      // Note - active low
+      uint snapshot =
+            (f1::read()?0:SW_F1)|
+            (f2::read()?0:SW_F2)|
+            (f3::read()?0:SW_F3)|
+            (f4::read()?0:SW_F4)|
+            (sel::read()?0:SW_S);
+
+      if ((snapshot != 0) && (snapshot == lastSnapshot)) {
+         debounceCount++;
+         if (debounceCount == DEBOUNCE_THRESHOLD) {
+            switchNum = (SwitchValue)snapshot;
+            repeating = false;
+         }
+         if ((debounceCount >= REPEAT_THRESHOLD) && ((debounceCount % REPEAT_PERIOD) == 0)) {
+            switchNum = (SwitchValue)snapshot;
+            repeating = true;
          }
       }
       else {
-         sw1Count = 0;
+         debounceCount = 0;
+         repeating = false;
       }
-      if (!f2::read()) {
-         sw2Count++;
-         if ((sw2Count == DEBOUNCE_THRESHOLD) || ((sw2Count >= REPEAT_THRESHOLD) && ((sw2Count % REPEAT_PERIOD) == 0))) {
-            switchNum = SW_F2;
-         }
-      }
-      else {
-         sw2Count = 0;
-      }
-      if (!f3::read()) {
-         sw3Count++;
-         if ((sw3Count == DEBOUNCE_THRESHOLD) || ((sw3Count >= REPEAT_THRESHOLD) && ((sw3Count % REPEAT_PERIOD) == 0))){
-            switchNum = SW_F3;
-         }
-      }
-      else {
-         sw3Count = 0;
-      }
-      if (!f4::read()) {
-         sw4Count++;
-         if ((sw4Count == DEBOUNCE_THRESHOLD) || ((sw4Count > REPEAT_THRESHOLD) && ((sw4Count % REPEAT_PERIOD) == 0))) {
-            switchNum = SW_F4;
-         }
-      }
-      else {
-         sw4Count = 0;
-      }
-      if (!sel::read()) {
-         sw5Count++;
-         if (sw5Count == DEBOUNCE_THRESHOLD) {
-            switchNum = SW_S;
-         }
-      }
-      else {
-         sw5Count = 0;
-      }
+      lastSnapshot  = snapshot;
    }
 
 public:
    /**
     * Initialise the switch monitoring
     */
-   static void initialise() {
+   SwitchDebouncer() {
       f1::setInput();
       f2::setInput();
       f3::setInput();
@@ -112,21 +98,31 @@ public:
       USBDM::Pit::setCallback(timerChannel, switchHandler);
       USBDM::Pit::enableInterrupts(timerChannel);
       switchNum = SW_NONE;
+      repeating = false;
    }
 
    /**
     * Check if a button is pressed without removing it from the buffer
     *
-    * @return button value or zero if none
+    * @return button value or SW_NONE if none pressed
     */
    static SwitchValue peekButton() {
       return switchNum;
    }
 
    /**
+    * Check if a button is repeating (i.e. button is held)
+    *
+    * @return True if repeating
+    */
+   static bool isRepeating() {
+      return repeating;
+   }
+
+   /**
     * Get button press
     *
-    * @return button value or zero if none pressed since last queried
+    * @return button value or SW_NONE if none pressed since last queried
     */
    static SwitchValue getButton() {
       disableInterrupts();
@@ -139,5 +135,8 @@ public:
 
 template<unsigned timerChannel, typename f1, typename f2, typename f3, typename f4, typename sel>
 volatile SwitchValue SwitchDebouncer<timerChannel, f1, f2, f3, f4, sel>::switchNum;
+
+template<unsigned timerChannel, typename f1, typename f2, typename f3, typename f4, typename sel>
+volatile bool SwitchDebouncer<timerChannel, f1, f2, f3, f4, sel>::repeating;
 
 #endif /* SOURCES_SWITCH_DEBOUNCER_H_ */
