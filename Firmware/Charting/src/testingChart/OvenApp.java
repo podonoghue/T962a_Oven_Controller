e1e08d481f9c2fb4bbbfd88f2885cde5f9f0a02d
podonoghue <podonoghue@swin.edu.au>
2017-03-27 20:56:53 +1100

    V1.1.0 Added profile retrieval and plotting

--- Firmware/Charting/src/testingChart/OvenApp.java
+++ Firmware/Charting/src/testingChart/OvenApp.java
@@ -39,6 +39,7 @@ public class OvenApp extends JFrame {
 
    static class SolderProfile {
       String   description;      // Description of the profile
+      int      flags;
       float    ramp1Slope;       // Slope up to soakTemp1
       float    soakTemp1;        // Temperature for start of soak
       float    soakTemp2;        // Temperature for end of soak
@@ -48,6 +49,9 @@ public class OvenApp extends JFrame {
       float    peakDwell;        // How long to remain at peakTemp
       float    rampDownSlope;    // Slope down after peakTemp
 
+      public SolderProfile() {
+
+      }
       public SolderProfile(
             String   description,      // Description of the profile
             float    ramp1Slope,       // Slope up to soakTemp1
@@ -77,7 +81,10 @@ public class OvenApp extends JFrame {
 
    private JPanel chartPanel;
    XYSeries profileSeries = new XYSeries("Profile");;
+   XYSeries targetSeries  = new XYSeries("Target");;
    XYSeries averageSeries = new XYSeries("Average");;
+   XYSeries fanSeries     = new XYSeries("Fan");;
+   XYSeries heaterSeries  = new XYSeries("Heater");;
 
    enum State {
       s_off,
@@ -103,7 +110,7 @@ public class OvenApp extends JFrame {
       float ambient = 25.0f;
       float time = 0.0f;
       profileSeries.add(time, ambient);
-      time += profile.soakTemp1/profile.ramp1Slope;
+      time += (profile.soakTemp1-ambient)/profile.ramp1Slope;
       profileSeries.add(time, profile.soakTemp1);
       time += profile.soakTime;
       profileSeries.add(time, profile.soakTemp2);
@@ -135,16 +142,11 @@ public class OvenApp extends JFrame {
    private XYDataset createDataset() {
       XYSeriesCollection dataset = new XYSeriesCollection();
 
-      plotProfile(profileSeries, profile);
-
-      averageSeries.add(20.0, 100.0);
-      averageSeries.add(20.5, 200.4);
-      averageSeries.add(30.2, 100.2);
-      averageSeries.add(30.9, 200.8);
-      averageSeries.add(40.6, 300.0);
-
       dataset.addSeries(profileSeries);
+      dataset.addSeries(targetSeries);
       dataset.addSeries(averageSeries);
+      dataset.addSeries(fanSeries);
+      dataset.addSeries(heaterSeries);
 
       return dataset;
    }
@@ -158,13 +160,13 @@ public class OvenApp extends JFrame {
 
       XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
 
-      // sets paint color for each series
-      renderer.setSeriesPaint(0, Color.RED);
-      renderer.setSeriesPaint(1, Color.GREEN);
-
-      // sets thickness for series (using strokes)
-      renderer.setSeriesStroke(0, new BasicStroke(4.0f));
-      renderer.setSeriesStroke(1, new BasicStroke(3.0f));
+      //      // sets paint color for each series
+      //      renderer.setSeriesPaint(0, Color.RED);
+      //      renderer.setSeriesPaint(1, Color.GREEN);
+      //
+      //      // sets thickness for series (using strokes)
+      //      renderer.setSeriesStroke(0, new BasicStroke(4.0f));
+      //      renderer.setSeriesStroke(1, new BasicStroke(3.0f));
 
       XYPlot plot = chart.getXYPlot();
 
@@ -182,12 +184,80 @@ public class OvenApp extends JFrame {
       //		axis.setRange(0.0, 600.0); // 600 seconds
       axis = plot.getRangeAxis();
       axis.setAutoRange(true);
-      axis.setRange(0.0, 400.0); 	// 0-400 C
+      //      axis.setRange(0.0, 400.0); 	// 0-400 C
       return new ChartPanel(chart);
    }
 
    int count = 100;
+   void updateProfileChart(SerialComManager scm, long handle) throws SerialComException {
+      scm.writeString(handle, "prof?\n\r", 0);
+      String data = scm.readString(handle);
+      String moreData;
+      do {
+         moreData = scm.readString(handle);
+         if (moreData == null) {
+            break;
+         }
+         data = data + moreData;
+      } while (true);
+      System.out.println("data read is :" + data);
+      String[] values = data.split(",");
+      if (values.length == 11) {
+         values[10] = values[10].split(";")[0];
+         SolderProfile profile = new SolderProfile();
+         profile.description   = values[1];
+         profile.flags         = Integer.valueOf(values[2], 16);
+         profile.ramp1Slope    = Float.parseFloat(values[3]);
+         profile.soakTemp1     = Integer.parseInt(values[4]);
+         profile.soakTemp2     = Integer.parseInt(values[5]);
+         profile.soakTime      = Integer.parseInt(values[6]);
+         profile.ramp2Slope    = Float.parseFloat(values[7]);
+         profile.peakTemp      = Integer.parseInt(values[8]);
+         profile.peakDwell     = Integer.parseInt(values[9]);
+         profile.rampDownSlope = Float.parseFloat(values[10]);
+         plotProfile(profileSeries, profile);
+      }
+   }
 
+   void updateOvenChart(SerialComManager scm, long handle) throws SerialComException {
+      scm.writeString(handle, "plot?\n\r", 0);
+      String data = scm.readString(handle);
+      String moreData;
+      do {
+         moreData = scm.readString(handle);
+         if (moreData == null) {
+            break;
+         }
+         data = data + moreData;
+      } while (true);
+      System.out.println("data read is :" + data);
+      String[] points = data.split(";");
+      averageSeries.clear();
+      heaterSeries.clear();
+      fanSeries.clear();
+      for (int index=1; index< points.length; index++) {
+         String[] values = points[index].split(",");
+         if (values.length == 10) {
+            String state         = values[0];
+            int    time          = Integer.parseInt(values[1]);
+            float  targetTemp    = Float.parseFloat(values[2]);
+            float  averageTemp   = Float.parseFloat(values[3]);
+            int    heaterPercent = Integer.parseInt(values[4]);
+            int    fanPercent    = Integer.parseInt(values[5]);
+            float  thermocouple1 = Float.parseFloat(values[6]);
+            float  thermocouple2 = Float.parseFloat(values[7]);
+            float  thermocouple3 = Float.parseFloat(values[8]);
+            float  thermocouple4 = Float.parseFloat(values[9]);
+            targetSeries.add((float)time, targetTemp);
+            averageSeries.add((float)time, averageTemp);
+            heaterSeries.add((float)time, (float)heaterPercent);
+            fanSeries.add((float)time, (float)fanPercent);
+            System.out.println(
+                  String.format("%3d %6.1f %6.1f %3d %3d ", 
+                        time, targetTemp, averageTemp, heaterPercent, fanPercent));
+         }
+      }
+   }
    /**
     * Handles a click on the button by adding new (random) data.
     *
@@ -205,9 +275,8 @@ public class OvenApp extends JFrame {
          String data = scm.readString(handle);
          System.out.println("data read is :" + data);
          if (data.startsWith("SMT-Oven")) {
-            scm.writeString(handle, "plot?\n\r", 0);
-            data = scm.readString(handle);
-            System.out.println("data read is :" + data);
+            updateOvenChart(scm, handle);
+            updateProfileChart(scm, handle);
          }
          scm.closeComPort(handle);
       } catch (Exception e) {
